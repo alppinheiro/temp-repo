0dbb33a93e0b81c7bf665cc1d823351047dcc4ae
alppinheiro <andre.l.pinheiro@hotmail.com>
2021-03-11 23:07:36 -0300

    Configuraçao do eslint, prettier, editorconfig e pro-types

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,13 +1,19 @@
+/* eslint-disable react/react-in-jsx-scope */
+import P from 'prop-types';
 import './styles.css';
-import { Component } from "react";
 
-export class Button extends Component {
-    render() {
-        const {text, onClick, disabled} = this.props;
-        return (
-            <button disabled={disabled} className='button' onClick={onClick}>
-                {text}
-            </button>
-        );
-    }
-}
+export const Button = ({ text, onClick, disabled = false }) => (
+  <button className="button" onClick={onClick} disabled={disabled}>
+    {text}
+  </button>
+);
+
+Button.defaultProps = {
+  disabled: false,
+};
+
+Button.propTypes = {
+  text: P.string.isRequired,
+  onClick: P.func.isRequired,
+  disabled: P.bool,
+};
--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -1,10 +1,20 @@
+/* eslint-disable react/react-in-jsx-scope */
+import P from 'prop-types';
 import './styles.css';
-export const TextInput = ({searchValue, handleChange}) => (
 
+export const TextInput = ({ searchValue, handleChange }) => {
+  return (
     <input
-    className="text-input"
-    value={searchValue}
-    type="search"
-    onChange={handleChange}
-    placeholder="Type your search" />
-)
+      className="text-input"
+      onChange={handleChange}
+      value={searchValue}
+      type="search"
+      placeholder="Type your search"
+    />
+  );
+};
+
+TextInput.propTypes = {
+  searchValue: P.string.isRequired,
+  handleChange: P.func.isRequired,
+};
